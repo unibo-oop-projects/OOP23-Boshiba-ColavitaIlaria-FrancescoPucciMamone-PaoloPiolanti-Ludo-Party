b32fec657328315f20b242a5f5e735883701152f
Danilo Pianini <danilo.pianini@unibo.it>
2024-02-27 16:17:32 +0100

    ci: add the OOP machinery

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,4 @@
+
 plugins {
     // Apply the java plugin to add support for Java
     java
@@ -13,7 +14,10 @@ plugins {
      */
     id("com.github.johnrengelman.shadow") version "8.1.1"
     id("org.danilopianini.gradle-java-qa") version "1.36.0"
+
+    id("org.danilopianini.unibo-oop-gradle-plugin") version "1.0.6"
 }
+
 
 repositories {
     mavenCentral()
@@ -66,3 +70,4 @@ application {
     // Define the main class for the application
     mainClass = "ludoparty.Main"
 }
+ java { toolchain { languageVersion.set(JavaLanguageVersion.of(17)) } }
